1a6b4db9e7e5252514cf0f24a14187df24de9db7
vickivv <wangquan94vicki@gmail.com>
2025-10-30 17:15:27 -0700

    exclude pyodide from vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ["pyodide"], // 👈 prevents Vite from pre-bundling Pyodide
+    exclude: ["pyodide"], // prevents Vite from pre-bundling Pyodide
   },
 });
 
